ba3b4bec4399193f49e5c7f04142fc1ba35556c8
wendyhamel <whamel@it4web.nl>
2022-06-08 10:06:16 +0200

    Working on styling

---
+++ src/tailwind.config.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'sans': ['Barlow Semi Condensed', 'sans-serif']
+        },
+        extend: {
+            colors: {
+                'paper-start' : 'hsl(230, 89%, 62%)',
+                'paper-end' : 'hsl(230, 89%, 65%)',
+                'rock-start' : 'hsl(349, 71%, 52%)',
+                'rock-end' : 'hsl(349, 70%, 56%)',
+                'scissors-start' : 'hsl(39, 89%, 49%)',
+                'scissors-end' : 'hsl(40, 84%, 53%)',
+                'lizard-start' : 'hsl(261, 73%, 60%)',
+                'lizard-end' : 'hsl(261, 72%, 63%)',
+                'cyan-start' : 'hsl(189, 59%, 53%)',
+                'cyan-end' : 'hsl(189, 58%, 57%)',
+
+                'dark' : 'hsl(229, 25%, 31%)',
+                'score' : 'hsl(229, 64%, 46%)',
+                'header-outline' : 'hsl(217, 16%, 45%)',
+
+                'radial-gradient-start' : 'hsl(214, 47%, 23%)',
+                'radial-gradient-end' : 'hsl(237, 49%, 15%)',
+            }
+        }
+    }
+}
